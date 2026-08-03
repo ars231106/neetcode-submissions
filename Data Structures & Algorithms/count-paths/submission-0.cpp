068ef703d7443f37adec28c091af6006c4d4777e
ars231106 <aryansurani32@gmail.com>
2026-08-03 15:59:00 +0530

    Update: count-paths - submission-0

--- Data Structures & Algorithms/count-paths/submission-0.cpp
+++ Data Structures & Algorithms/count-paths/submission-0.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n , 0));
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        vector<vector<unsigned int>> dp(n + 1, vector<unsigned int>(amount + 1, 0));
 
-        for(int i = 0; i<m; i++){
+        for (int i = 0; i <= n; i++) {
             dp[i][0] = 1;
         }
 
-        for(int j = 0; j<n; j++){
-            dp[0][j] = 1;
-        }
-
-        for(int i = 1; i<m; i++){
-            for(int j = 1; j<n;j++){
-                dp[i][j] = dp[i][j-1] + dp[i-1][j];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= amount; j++) {
+                if (coins[i - 1] > j) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]];
+                }
             }
         }
 
-        return dp[m-1][n-1];
+        return (int)dp[n][amount];
     }
 };
